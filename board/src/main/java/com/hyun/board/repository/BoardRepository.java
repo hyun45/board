91c83577dba9cc11357334abdcd7460a81061921
45183 <hsb9743@naver.com>
2024-03-19 09:42:40 +0900

    게시판 CRUD

---
+++ board/src/main/java/com/hyun/board/repository/BoardRepository.java
@@ -0,0 +1,37 @@
+package com.hyun.board.repository;
+
+import com.hyun.board.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.mybatis.spring.SqlSessionTemplate;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+    private final SqlSessionTemplate sql;
+    public void save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO); // 하나의 파라미터만 가능
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
+    }
+
+    public void updateHits(Long id) {
+        sql.update("Board.updateHits", id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return sql.selectOne("Board.findById", id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        sql.update("Board.update", boardDTO);
+    }
+
+    public void delete(Long id) {
+        sql.delete("Board.delete", id);
+    }
+}
---
+++ board/src/main/java/com/hyun/board/service/BoardService.java
@@ -0,0 +1,39 @@
+package com.hyun.board.service;
+
+import com.hyun.board.dto.BoardDTO;
+import com.hyun.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO) {
+        boardRepository.save(boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+    }
+
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return boardRepository.findById(id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        boardRepository.update(boardDTO);
+    }
+
+    public void delete(Long id) {
+        boardRepository.delete(id);
+    }
+}
